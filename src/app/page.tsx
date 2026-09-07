c7ce5f816c96662853577483512733e11928d22c
Ranmini <sandaliranmini2000@gmail.com>
2026-09-07 18:40:09 +0530

    connect the nava bar to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import YourVirtualGateway from "@/components/home/your-virtual-gateway"
 import FeaturedDestination from "@/components/home/featured-destinations";
+import Navbar from "@/components/layout/navbar";
 
 export default function HomePage(){
     return (
         <>
+
+<Navbar/>
 
 <YourVirtualGateway/>
 
